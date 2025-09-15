72c05e18941123f1c50b11e3e24634e5a255af8b
mpotociar <potociarmartin@gmail.com>
2025-09-15 22:26:27 +0200

    feat(section1-task-A): refactor VPC subnet allocation to use local variables

--- section-1/task-A/terraform/modules/aws_vpc/main.tf
+++ section-1/task-A/terraform/modules/aws_vpc/main.tf
@@ -1,37 +1,20 @@
-data "aws_availability_zones" "available" {}
-data "aws_region" "current" {}
-
 resource "aws_vpc" "this" {
   cidr_block = var.vpc_cidr
 }
 
-# Create an IPAM pool for the VPC
-resource "aws_vpc_ipam" "ipam" {
-  operating_regions {
-    region_name = data.aws_region.current.region
-  }
-}
-
-resource "aws_vpc_ipam_pool" "ipam_pool" {
-  address_family = "ipv4"
-  ipam_scope_id  = aws_vpc_ipam.ipam.private_default_scope_id
-}
-
-# Allocate /24 CIDRs for each public and private subnet per AZ
-resource "aws_vpc_ipam_pool_cidr" "public" {
-  for_each = { for idx, az in var.azs : idx => az }
-  ipam_pool_id = aws_vpc_ipam_pool.ipam_pool.id
-  netmask_length  = 24
-}
-
-resource "aws_vpc_ipam_pool_cidr" "private" {
-  for_each = { for idx, az in var.azs : idx => az }
-  ipam_pool_id = aws_vpc_ipam_pool.ipam_pool.id
-  netmask_length  = 24
-}
-
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.this.id
+}
+
+# Calculate subnet CIDRs using cidrsubnet function
+# For each AZ, create public subnet (starting from index 0) and private subnet (starting from index for length of azs)
+locals {
+  public_subnet_cidrs = {
+    for idx, az in var.azs : idx => cidrsubnet(var.vpc_cidr, 8, idx)
+  }
+  private_subnet_cidrs = {
+    for idx, az in var.azs : idx => cidrsubnet(var.vpc_cidr, 8, idx + length(var.azs))
+  }
 }
 
 module "az_networks" {
@@ -40,7 +23,7 @@ module "az_networks" {
 
   az                  = each.value
   vpc_id              = aws_vpc.this.id
-  public_subnet_cidr  = aws_vpc_ipam_pool_cidr.public[each.key].cidr
-  private_subnet_cidr = aws_vpc_ipam_pool_cidr.private[each.key].cidr
+  public_subnet_cidr  = local.public_subnet_cidrs[each.key]
+  private_subnet_cidr = local.private_subnet_cidrs[each.key]
   igw_id              = aws_internet_gateway.gw.id
 }
